0a1dda11391501697ba50c33a7cfe26c99f246e6
editid0 <66117959+editid0@users.noreply.github.com>
2021-08-24 16:29:39 +0200

    Update install.sh

--- pgadmin/install.sh
+++ pgadmin/install.sh
@@ -1,7 +1,4 @@
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add -y
-touch a.txt
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-touch b.txt
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update -y'
 sudo apt install -y pgadmin4-web
-touch c.txt
 echo "run 'sudo /usr/pgadmin4/bin/setup-web.sh'"
