d778848a6937bb2167f94ead5d0400761d238b1a
editid0 <66117959+editid0@users.noreply.github.com>
2021-10-02 14:49:32 +0100

    Update install.sh

--- ffmpeg/install.sh
+++ ffmpeg/install.sh
@@ -1,4 +1,4 @@
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo apt update
-sudo add-apt-repository ppa:jonathonf/ffmpeg-4
+sudo add-apt-repository ppa:jonathonf/ffmpeg-4 -y
 sudo apt install ffmpeg -y
