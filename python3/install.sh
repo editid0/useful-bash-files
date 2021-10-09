d1d750d6f412f49c83693f95ce5b043e31da4f42
editid0 <66117959+editid0@users.noreply.github.com>
2021-10-09 19:47:27 +0100

    Update install.sh

--- python3/install.sh
+++ python3/install.sh
@@ -1,4 +1,4 @@
-sudo apt install software-properties-common
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install software-properties-common -y
+sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt update
 sudo apt install python3.8 -y
